96ab8cfd2a6174d31f2bd55cf324b516a56d536e
Michael Lin <michael.lin2k21@gmail.com>
2026-02-05 12:56:45 -0500

    skeleton for motion_model

--- hw1/code/motion_model.py
+++ hw1/code/motion_model.py
@@ -29,10 +29,20 @@ class MotionModel:
         """
         param[in] u_t0 : particle state odometry reading [x, y, theta] at time (t-1) [odometry_frame]
         param[in] u_t1 : particle state odometry reading [x, y, theta] at time t [odometry_frame]
-        param[in] x_t0 : particle state belief [x, y, theta] at time (t-1) [world_frame]
-        param[out] x_t1 : particle state belief [x, y, theta] at time t [world_frame]
+        param[in] x_t0 : particle state belief [x, y, theta] at time (t-1) [world_frame]; has shape [num_particles, 3]
+        param[out] x_t1 : particle state belief [x, y, theta] at time t [world_frame]; has shape [num_particles, 3]
         """
         """
         TODO : Add your code here
         """
-        return np.random.rand(3)
+
+        # (1) estimate displacement in odometry frame
+        du = np.array([u_t1[0] - u_t0[0], u_t1[1] - u_t0[1], u_t1[2] - u_t0[2]])
+
+        # (2) expand to [num_particles, 3] and add some estimated noise
+        du_expanded = np.expand_dims(0, du).repeat(x_t0.shape[0])
+        du_noisy = du_expanded  # TODO
+
+        # (3) utilize displacement in odometry frame to calculate displacement in world frame
+        x_t1 = x_t0 + du_noisy
+        return x_t1
