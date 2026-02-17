prelminary fixes to motion_model to reduce variance

hw1/code/main.py
@@ -56,11 +56,28 @@ def init_particles_random(num_particles, occupancy_map):
 def init_particles_freespace(num_particles, occupancy_map):
 
     # initialize [x, y, theta] positions in world_frame for all particles
-    """
-    TODO : Add your code here
-    This version converges faster than init_particles_random
-    """
-    X_bar_init = np.zeros((num_particles, 4))
+    # occupancy_map values:
+    #   -1: unknown
+    #    0: free
+    #    1: occupied
+    # Treat low occupancy probabilities as free space and sample only from those cells.
+    free_threshold = 0.10
+    free_mask = (occupancy_map >= 0.0) & (occupancy_map <= free_threshold)
+    free_cells = np.argwhere(free_mask)[:, ::-1]  #argwhere default is row, col, we want x,y
+
+
+    # Sample free cells with replacement; map resolution is 10 cm per cell.
+    chosen = free_cells[np.random.choice(free_cells.shape[0], size=num_particles, replace=True)]
+    resolution_cm = 10.0
+    x0_vals = (chosen[:, 0:1] + np.random.rand(num_particles, 1)) * resolution_cm
+    y0_vals = (chosen[:, 1:2] + np.random.rand(num_particles, 1)) * resolution_cm
+    theta0_vals = np.random.uniform(-np.pi, np.pi, (num_particles, 1))
+
+    # initialize weights for all particles
+    w0_vals = np.ones((num_particles, 1), dtype=np.float64)
+    w0_vals = w0_vals / num_particles
+
+    X_bar_init = np.hstack((x0_vals, y0_vals, theta0_vals, w0_vals))
 
     return X_bar_init
 
@@ -82,7 +99,8 @@ if __name__ == '__main__':
     parser.add_argument('--path_to_map', default='../data/map/wean.dat')
     parser.add_argument('--path_to_log', default='../data/log/robotdata1.log')
     parser.add_argument('--output', default='results')
-    parser.add_argument('--batch_size', default=10, type=int)
+    parser.add_argument('--batch_size', default=500, type=int)
+    parser.add_argument('--subsampling', default=3, type=int)
     parser.add_argument('--num_particles', default=500, type=int)
     parser.add_argument('--visualize', action='store_true')
     args = parser.parse_args()
@@ -97,12 +115,12 @@ if __name__ == '__main__':
 
     motion_model = MotionModel()
     # sensor_model = SensorModel(occupancy_map)
-    sensor_model = SensorModel(map_obj, batch_size=args.batch_size)  #changed
+    sensor_model = SensorModel(map_obj, subsampling=args.subsampling, batch_size=args.batch_size)  #changed
     resampler = Resampling()
 
     num_particles = args.num_particles
-    X_bar = init_particles_random(num_particles, occupancy_map)
-    # X_bar = init_particles_freespace(num_particles, occupancy_map)
+    # X_bar = init_particles_random(num_particles, occupancy_map)
+    X_bar = init_particles_freespace(num_particles, occupancy_map)
 
     batch_size = args.batch_size
 

hw1/code/motion_model.py
@@ -36,9 +36,9 @@ class MotionModel:
         """
 
         # (1) calculate standard deviations of noise Gaussians
-        std_rot1 = self._alpha1 * d_rot1 ** 2 + self._alpha2 * d_trans ** 2
-        std_trans = self._alpha3 * (d_rot1 ** 2 + d_rot2 ** 2) + self._alpha4 * d_trans ** 2
-        std_rot2 = self._alpha1 * d_rot2 ** 2 + self._alpha2 * d_trans ** 2
+        std_rot1 = self._alpha1 * np.abs(d_rot1) + self._alpha2 * np.abs(d_trans)
+        std_trans = self._alpha3 * (np.abs(d_rot1) + np.abs(d_rot2)) + self._alpha4 * np.abs(d_trans)
+        std_rot2 = self._alpha1 * np.abs(d_rot2) + self._alpha2 * np.abs(d_trans)
 
         # (2) sample noise from distributions. We don't need to scale since \sigma already depends on the translation 
         noise_rot1 = np.random.normal(loc=0, scale=std_rot1, size=batch_size)
@@ -88,6 +88,9 @@ class MotionModel:
         d_trans = np.sqrt(dy**2 + dx**2)
         d_rot2 = dtheta - d_rot1
 
+        d_rot1 = np.arctan2(np.sin(d_rot1), np.cos(d_rot1))
+        d_rot2 = np.arctan2(np.sin(d_rot2), np.cos(d_rot2))
+
         # (2) add sampled noise to estimated rotations and translation
         x_noise = self.sample(d_rot1, d_trans, d_rot2, x_t0.shape[0])
         du_noisy = np.array([d_rot1, d_trans, d_rot2])[np.newaxis, :] + x_noise
@@ -97,6 +100,7 @@ class MotionModel:
         du_x = du_trans * np.cos(x_t0[:, 2] + du_rot1)
         du_y = du_trans * np.sin(x_t0[:, 2] + du_rot1)
         du_theta = du_rot1 + du_rot2
+        du_theta = np.arctan2(np.sin(du_theta), np.cos(du_theta))
         x_t1 = x_t0 + np.stack([du_x, du_y, du_theta], axis=-1)
         
         return x_t1